b060113f08dcd04887eff53535dd32ae6147d734
Gowrisankar Samy <gsankar22@gmail.com>
2024-11-19 16:58:38 +0530

    Initial Commit

---
+++ GMartTests/GMartTests.swift
@@ -0,0 +1,17 @@
+//
+//  GMartTests.swift
+//  GMartTests
+//
+//  Created by Gowrisankar Samy on 19/11/24.
+//
+
+import Testing
+@testable import GMart
+
+struct GMartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
